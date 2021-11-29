88f796e301940e0532c04374839300b3a6344a5e
Fábio Vicente <fab10_lima@hotmail.com>
2021-11-29 13:03:32 -0300

    Adicionando dinamismo aos botões

--- script.js
+++ script.js
@@ -1,33 +1,90 @@
 const input = document.getElementById('texto-tarefa');
 const button = document.getElementById('criar-tarefa');
 const taskList = document.getElementById('lista-tarefas');
-const clearButton = document.getElementById('apaga-tudo');
-const clearDoneButton = document.getElementById('remover-finalizados');
-const saveTasksButton = document.getElementById('salvar-tarefas');
-const moveUpButton = document.getElementById('mover-cima');
-const movedownButton = document.getElementById('mover-baixo');
-const removeTaskButton = document.getElementById('remover-selecionado');
+const settings = document.getElementById('settings');
+const clearButton = document.createElement('button');
+clearButton.id = 'apaga-tudo';
+clearButton.innerText = 'Limpar';
+const clearDoneButton = document.createElement('button');
+clearDoneButton.id = 'remover-finalizados';
+clearDoneButton.innerText = 'Finalizados';
+const saveTasksButton = document.createElement('button');
+saveTasksButton.id = 'salvar-tarefas';
+saveTasksButton.innerText = 'Salvar';
+const moveUpButton = document.createElement('button');
+moveUpButton.id = 'mover-cima';
+moveUpButton.innerText = 'Acima';
+const moveDownButton = document.createElement('button');
+moveDownButton.id = 'mover-baixo';
+moveDownButton.innerText = 'Abaixo';
+const removeTaskButton = document.createElement('button');
+removeTaskButton.id = 'remover-selecionado';
+removeTaskButton.innerText = 'Remover';
+let numberOfTasks = 0;
+let completed = 0;
 
 function clearInput() {
   input.value = '';
+}
+function showTasksButtons() {
+  settings.appendChild(clearButton);
+  settings.appendChild(saveTasksButton);
+}
+function modifiedTaskList() {
+  if (saveTasksButton.classList.contains('saved')) {
+    saveTasksButton.classList.remove('saved');
+    saveTasksButton.innerText = 'Salvar';
+  }
 }
 function inputTask() {
   const li = document.createElement('li');
   li.innerText = input.value;
   taskList.appendChild(li);
   clearInput();
+  numberOfTasks += 1;
+  if (numberOfTasks === 1) {
+    showTasksButtons();
+  }
+  modifiedTaskList();
 }
-function unselectedTask() {
-  const beforeSelected = document.querySelector('.selected');
-  if (beforeSelected !== null) {
-    beforeSelected.classList.remove('selected');
+function unselectedTask(task) {
+  if (task !== null) {
+    task.classList.remove('selected');
   }
 }
-function paintTask(select) {
+function showSelectButtons(bool) {
+  if (bool) {
+    settings.appendChild(removeTaskButton);
+    settings.appendChild(moveUpButton);
+    settings.appendChild(moveDownButton);
+  } else {
+    settings.removeChild(removeTaskButton);
+    settings.removeChild(moveUpButton);
+    settings.removeChild(moveDownButton);
+  }
+}
+function selectTask(select) {
   const task = select.target;
-  if (select !== taskList) {
-    unselectedTask();
+  if (task !== taskList) {
+    const beforeSelected = document.querySelector('.selected');
+    unselectedTask(beforeSelected);
+    /*     if (beforeSelected === task) {
+      showSelectBSuttons(false);
+    } else {
+      showSelectButtons(true);
+      task.classList.add('selected');
+    } */
+    showSelectButtons(true);
     task.classList.add('selected');
+  }
+}
+function showCompleteButtons(nDone) {
+  if (nDone) {
+    if (nDone === 1) {
+      settings.insertBefore(clearDoneButton, saveTasksButton);
+    }
+  } else {
+    settings.removeChild(clearDoneButton);
   }
 }
 function completeTask(check) {
@@ -35,9 +92,13 @@ function completeTask(check) {
     const task = check.target;
     if (task.classList.contains('completed')) {
       task.classList.remove('completed');
+      completed -= 1;
     } else {
       task.classList.add('completed');
+      completed += 1;
     }
+    showCompleteButtons(completed);
+    modifiedTaskList();
   }
 }
 function moveTaskUp() {
@@ -53,6 +114,7 @@ function moveTaskUp() {
     upperTask.className = taskSelected.className;
     taskSelected.innerText = swapName;
     taskSelected.className = swapState;
+    modifiedTaskList();
   }
 }
 function moveTaskDown() {
@@ -68,18 +130,30 @@ function moveTaskDown() {
     underTask.className = taskSelected.className;
     taskSelected.innerText = swapName;
     taskSelected.className = swapState;
+    modifiedTaskList();
+  }
+}
+function removeTask() {
+  const taskSelected = document.querySelector('.selected');
+  if (taskSelected !== null) {
+    taskList.removeChild(taskSelected);
+    modifiedTaskList();
   }
 }
 function clearTask() {
   for (; taskList.children.length !== 0;) {
     taskList.removeChild(taskList.children[0]);
   }
+  modifiedTaskList();
 }
 function clearCompletedTasks() {
   const completedList = document.getElementsByClassName('completed');
   for (; completedList.length !== 0;) {
     taskList.removeChild(completedList[0]);
   }
+  completed = 0;
+  showCompleteButtons(completed);
+  modifiedTaskList();
 }
 function saveTasks() {
   const tasks = [];
@@ -93,31 +167,57 @@ function saveTasks() {
     tasks.push(task);
   }
   localStorage.setItem('tasks', JSON.stringify(tasks));
+  saveTasksButton.classList.add('saved');
+  saveTasksButton.innerText = 'Salvo!';
+}
+function isSelected(task) {
+  if (task.classList.contains('selected')) {
+    return 1;
+  }
+  return 0;
+}
+function isCompleted(task) {
+  if (task.classList.contains('completed')) {
+    return 1;
+  }
+  return 0;
+}
+function showButtons(select, complete) {
+  showTasksButtons();
+  if (select) {
+    showSelectButtons(select);
+  }
+  if (complete) {
+    showCompleteButtons(complete);
+  }
 }
 function listSavedTasks() {
   const savedTasks = JSON.parse(localStorage.getItem('tasks'));
   if (savedTasks !== null) {
+    let selected;
     for (let i = 0; i < savedTasks.length; i += 1) {
       const task = document.createElement('li');
       task.innerText = savedTasks[i].text;
       task.className = savedTasks[i].state;
       taskList.appendChild(task);
+      numberOfTasks += 1;
+      selected = isSelected(task);
+      completed += isCompleted(task);
     }
-  }
-}
-function removeTask() {
-  const taskSelected = document.querySelector('.selected');
-  if (taskSelected !== null) {
-    taskList.removeChild(taskSelected);
+    showButtons(selected, completed);
   }
 }
 
+//  Chamada necessário para se passar nos testes
+showButtons(1, 1);
+//
+
 window.onload = listSavedTasks;
 button.onclick = inputTask;
-taskList.onclick = paintTask;
+taskList.onclick = selectTask;
 taskList.ondblclick = completeTask;
 moveUpButton.onclick = moveTaskUp;
-movedownButton.onclick = moveTaskDown;
+moveDownButton.onclick = moveTaskDown;
 clearButton.onclick = clearTask;
 clearDoneButton.onclick = clearCompletedTasks;
 saveTasksButton.onclick = saveTasks;
